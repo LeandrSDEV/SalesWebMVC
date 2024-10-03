da17dca6f9f65b2d13bd65998f4d0e662c36a428
LEANDRO <crazyg4ng@gmail.com>
2024-10-03 06:31:54 -0300

    Populando com a classe SeedingService

--- Program.cs
+++ Program.cs
@@ -9,10 +9,12 @@ namespace SalesWebMVC
         {
             var builder = WebApplication.CreateBuilder(args);
             builder.Services.AddDbContext<BancoContext>(options =>
-                options.UseMySql(builder.Configuration.GetConnectionString("DataBase"), new MySqlServerVersion(new Version(8,0,39))));
+                options.UseMySql(builder.Configuration.GetConnectionString("DataBase"), new MySqlServerVersion(new Version(8, 0, 39))));
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddScoped<SeedingService>();
 
             var app = builder.Build();
 
@@ -22,6 +24,12 @@ namespace SalesWebMVC
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+            }
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var seedingService = scope.ServiceProvider.GetRequiredService<SeedingService>();
+                seedingService.Seed();
             }
 
             app.UseHttpsRedirection();
